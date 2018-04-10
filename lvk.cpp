9c64d54733fca98d6e3ed80e0f4c9e45feb8429a
Ivan Garkavy <jimmycalling@yandex.ru>
2018-04-10 21:54:50 +0300

    added sysSaveToXml func

--- lvk.cpp
+++ lvk.cpp
@@ -74,8 +74,8 @@ public:
 	}
 	void pushBackEmptyModule() {
 		modules.push_back(Module());
-		cout << "pushed back empty module, hwsize = " <<     //DEBUG
-			modules[modules.size() - 1].hardVersions.size() << endl; //DEBUG
+		//cout << "pushed back empty module, hwsize = " <<     //DEBUG
+		//	modules[modules.size() - 1].hardVersions.size() << endl; //DEBUG
 	}
 	void pushBackHardVersion(int moduleNo, double rel, double cost) {
 		modules[moduleNo - 1].hardVersions.push_back(Hardware(rel, cost));
@@ -133,13 +133,13 @@ void sysGenFromXml(System& system, const char* filename) {
 //непрерывно монотонно, начиная с 1
 	pugi::xml_document doc;
     pugi::xml_parse_result result = doc.load_file(filename);
-    cout << "Load result: " << result.description() << endl; //DEBUG
+    //cout << "Load result: " << result.description() << endl; //DEBUG
     if(!result)
 		throw PugiXmlExc(result);
     pugi::xml_node sysNode = doc;
     sysNode = sysNode.child("system");
     system.limitCost() = stod( sysNode.attribute("limitcost").value() );
-    cout << "sys limitCost = " << system.limitCost() << endl; //DEBUG
+    //cout << "sys limitCost = " << system.limitCost() << endl; //DEBUG
     int i = 1;
     for(pugi::xml_node iNode = sysNode.child("module"); 
 			iNode;
@@ -147,26 +147,26 @@ void sysGenFromXml(System& system, const char* filename) {
 	{
 		system.pushBackEmptyModule();
 		const char* wareType[2] = {"hw", "sw"};
-		cout << "in i-cycle\n"; //DEBUG
+		//cout << "in i-cycle\n"; //DEBUG
 		for(int k = 0; k < 2; k++) {
 			for(pugi::xml_node jNode = iNode.child(wareType[k]);
 					jNode;
 					jNode = jNode.next_sibling(wareType[k]) )
 			{
-				cout << "in j-cycle1\n"; //DEBUG
+				//cout << "in j-cycle1\n"; //DEBUG
 				string str1 = jNode.attribute("rel").value();
 				double rel = stod(str1);
 				string str2 = jNode.attribute("cost").value();
 				double cost = stod(str2);
-				cout << "in j-cycle2\n"; //DEBUG
+				//cout << "in j-cycle2\n"; //DEBUG
 				if(k == 0) {
-					cout << "in j-cycle3h\n"; //DEBUG
+					//cout << "in j-cycle3h\n"; //DEBUG
 					system.pushBackHardVersion(i, rel, cost);
-					cout << "in j-cycle4h\n"; //DEBUG
+					//cout << "in j-cycle4h\n"; //DEBUG
 				} else {
-					cout << "in j-cycle3s\n"; //DEBUG
+					//cout << "in j-cycle3s\n"; //DEBUG
 					system.pushBackSoftVersion(i, rel, cost);
-					cout << "in j-cycle4s\n"; //DEBUG
+					//cout << "in j-cycle4s\n"; //DEBUG
 				}
 			}
 		}
@@ -178,70 +178,59 @@ void System::printTest() const { //DEBUG
 	int n = modules.size();
 	for(int i = 0; i < n; i++) {
 		cout << "module " << i << ":" << endl;
-		int nHard = modules[i].hardVersions.size();
-		for(int j = 0; j < nHard; j++) {
-			cout << "	hard " << j << ": rel = " <<
-				modules[i].hardVersions[j].rel << ", cost = " <<
-				modules[i].hardVersions[j].cost << endl;
-		}
 		int nSoft = modules[i].softVersions.size();
 		for(int j = 0; j < nSoft; j++) {
 			cout << "	soft " << j << ": rel = " <<
 				modules[i].softVersions[j].rel << ", cost = " <<
 				modules[i].softVersions[j].cost << endl;
 		}
+		int nHard = modules[i].hardVersions.size();
+		for(int j = 0; j < nHard; j++) {
+			cout << "	hard " << j << ": rel = " <<
+				modules[i].hardVersions[j].rel << ", cost = " <<
+				modules[i].hardVersions[j].cost << endl;
+		}
 	}
 }
 
-/*
 void sysSaveToXml(System& system, const char* filename) {
 //сохраняет данные о системе и всех версиях оборудования
 	pugi::xml_document doc;
-    pugi::xml_parse_result result = doc.load_file(filename);
-    cout << "Load result: " << result.description() << endl; //DEBUG
-    if(!result)
-		throw PugiXmlExc(result);
-    pugi::xml_node sysNode = doc;
-    sysNode = sysNode.child("system");
-    system.limitCost() = stod( sysNode.attribute("limitcost").value() );
-    cout << "sys limitCost = " << system.limitCost() << endl; //DEBUG
-    int i = 1;
-    for(pugi::xml_node iNode = sysNode.child("module"); 
-			iNode;
-			iNode = iNode.next_sibling("module"), i++ )
-	{
-		system.pushBackEmptyModule();
-		const char* wareType[2] = {"hw", "sw"};
-		cout << "in i-cycle\n"; //DEBUG
-		for(int k = 0; k < 2; k++) {
-			for(pugi::xml_node jNode = iNode.child(wareType[k]);
-					jNode;
-					jNode = jNode.next_sibling(wareType[k]) )
-			{
-				cout << "in j-cycle1\n"; //DEBUG
-				string str1 = jNode.attribute("rel").value();
-				double rel = stod(str1);
-				string str2 = jNode.attribute("cost").value();
-				double cost = stod(str2);
-				cout << "in j-cycle2\n"; //DEBUG
-				if(k == 0) {
-					cout << "in j-cycle3h\n"; //DEBUG
-					system.pushBackHardVersion(i, rel, cost);
-					cout << "in j-cycle4h\n"; //DEBUG
-				} else {
-					cout << "in j-cycle3s\n"; //DEBUG
-					system.pushBackSoftVersion(i, rel, cost);
-					cout << "in j-cycle4s\n"; //DEBUG
-				}
-			}
+    pugi::xml_node sysNode = doc.append_child("system");
+    sysNode.append_attribute("limitcost") = system.limitCost();
+    int nModules = system.getNModules();
+    for(int i = 1; i <= nModules; i++) {
+		pugi::xml_node moduleNode = sysNode.append_child("module");
+		moduleNode.append_attribute("num") = i;
+		int nSoft = system.getNSoftVersions(i);
+		for(int j = 1; j <= nSoft; j++) {
+			pugi::xml_node wareNode = moduleNode.append_child("sw");
+			wareNode.append_attribute("num") = j;
+			char buf[64];
+			sprintf(buf, "%.3lf", system.getSoftVersion(i, j).rel);
+			wareNode.append_attribute("rel") = buf;
+			sprintf(buf, "%.3g", system.getSoftVersion(i, j).cost);
+			wareNode.append_attribute("cost") = buf;
 		}
-	}	
+		int nHard = system.getNHardVersions(i);
+		for(int j = 1; j <= nHard; j++) {
+			pugi::xml_node wareNode = moduleNode.append_child("hw");
+			wareNode.append_attribute("num") = j;
+			char buf[64];
+			sprintf(buf, "%.3lf", system.getHardVersion(i, j).rel);
+			wareNode.append_attribute("rel") = buf;
+			sprintf(buf, "%.3g", system.getHardVersion(i, j).cost);
+			wareNode.append_attribute("cost") = buf;
+		}
+	}
+    //doc.print(cout); //DEBUG
+    doc.save_file(filename);
 }
-*/
 
 int main(int argc, const char** argv) {
 	System system;
 	sysGenFromXml(system, "example.xml");
 	system.printTest();
+	sysSaveToXml(system, "out.xml");
 	return 0;
 }
