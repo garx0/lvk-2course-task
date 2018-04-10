25d8b070a4105b779bf3e7d8f510cea08f5263dc
Ivan Garkavy <jimmycalling@yandex.ru>
2018-04-10 23:55:11 +0300

    rewrited all functions to sw/hw enum format

--- lvk.cpp
+++ lvk.cpp
@@ -28,27 +28,41 @@ public:
 class System {
 public:
 	struct Ware {
+		enum class Type { SW, HW };
+		//static char* const wareTypeStr[] = {"sw", "hw"};
+		static int typeToInt(Type type) {
+			return static_cast<int>(type);
+		}
+		static Type intToType(int num) {
+			if (num <= 0) {return Type::SW;} else {return Type::HW;}
+		}
 		friend System;
 		double rel;
 		double cost;
 		Ware(): rel(1.0), cost(0.0) {}
 		Ware(double a_rel, double a_cost): rel(a_rel), cost(a_cost) {}
 	};
-	struct Hardware : Ware {
-		friend System;
-		Hardware(double rel, double cost): Ware(rel, cost) {}
-	};
+	//const char* const Ware::wareTypeStr[] = {"sw", "hw"};
+	//const char * const Ware::wareTypeStr[] = {"sw", "hw"};
 	struct Software : Ware {
 		friend System;
+		static const Type type = Type::SW;
+		Software(): Ware() {}
 		Software(double rel, double cost): Ware(rel, cost) {}
+	};
+	struct Hardware : Ware {
+		friend System;
+		static const Type type = Type::HW;
+		Hardware(): Ware() {}
+		Hardware(double rel, double cost): Ware(rel, cost) {}
 	};
 private:
 	class Module {
 		friend System;
-		vector<Hardware> hardVersions;
 		vector<Software> softVersions;
-		int curHardVersionNo_;
-		int curSoftVersionNo_;
+		vector<Hardware> hardVersions;
+		int curSoftVersionNo;
+		int curHardVersionNo;
 	};
 private:
 	vector<Module> modules;
@@ -60,47 +74,16 @@ private:
 	//	нумерация версий и модулей идет от 1
 public:
 	int getNModules() const {return modules.size();}
-	int getNHardVersions(int moduleNo) const {
-		return modules[moduleNo - 1].hardVersions.size();
-	}
-	int getNSoftVersions(int moduleNo) const {
-		return modules[moduleNo - 1].softVersions.size();
-	}
-	const Hardware& getHardVersion(int moduleNo, int versionNo) const {
-		return modules[moduleNo - 1].hardVersions[versionNo - 1];
-	}
-	const Software& getSoftVersion(int moduleNo, int versionNo) const {
-		return modules[moduleNo - 1].softVersions[versionNo - 1];
-	}
-	void pushBackEmptyModule() {
-		modules.push_back(Module());
-		//cout << "pushed back empty module, hwsize = " <<     //DEBUG
-		//	modules[modules.size() - 1].hardVersions.size() << endl; //DEBUG
-	}
-	void pushBackHardVersion(int moduleNo, double rel, double cost) {
-		modules[moduleNo - 1].hardVersions.push_back(Hardware(rel, cost));
-	}	
-	void pushBackSoftVersion(int moduleNo, double rel, double cost) {
-		modules[moduleNo - 1].softVersions.push_back(Software(rel, cost));
-	}	
-	int& curHardVersionNo(int moduleNo) {
-		return modules[moduleNo - 1].curHardVersionNo_;
-	}
-	int curHardVersionNo(int moduleNo) const {
-		return modules[moduleNo - 1].curHardVersionNo_;
-	}
-	int& curSoftVersionNo(int moduleNo) {
-		return modules[moduleNo - 1].curSoftVersionNo_;
-	}
-	int curSoftVersionNo(int moduleNo) const {
-		return modules[moduleNo - 1].curSoftVersionNo_;
-	}
-	const Hardware& getCurHardVersion(int moduleNo) const {
-		return getHardVersion(moduleNo, curHardVersionNo(moduleNo));
-	}
-	const Software& getCurSoftVersion(int moduleNo) const {
-		return getSoftVersion(moduleNo, curSoftVersionNo(moduleNo));
-	}
+	int getNWareVersions(int moduleNo, Ware::Type wareType) const;
+	const Ware& getWareVersion(int moduleNo, Ware::Type wareType, 
+		int versionNo) const;
+	void pushBackEmptyModule();
+	void pushBackWareVersion(int moduleNo, Ware::Type wareType, 
+		double rel, double cost);
+	int& curWareVersionNo(int moduleNo, Ware::Type wareType);
+	int curWareVersionNo(int moduleNo, Ware::Type wareType) const;
+	const Ware& getCurWareVersion(int moduleNo, 
+		Ware::Type wareType) const;
 	double getRel();
 	double getCost();
 	const double& limitCost() const {return limitCost_;}
@@ -108,12 +91,76 @@ public:
 	void printTest() const; //DEBUG
 };
 
+//using namespace System::Ware;
+
+int System::getNWareVersions(int moduleNo, Ware::Type wareType) const {
+	switch(wareType) {
+		case Ware::Type::SW:
+			return modules[moduleNo - 1].softVersions.size();
+		case Ware::Type::HW:
+			return modules[moduleNo - 1].hardVersions.size();
+	}
+}
+
+const System::Ware& System::getWareVersion(int moduleNo, 
+		Ware::Type wareType, int versionNo) const {
+	switch(wareType) {
+		case System::Ware::Type::SW:
+			return modules[moduleNo - 1].softVersions[versionNo - 1];
+		case System::Ware::Type::HW:
+			return modules[moduleNo - 1].hardVersions[versionNo - 1];
+	}
+}
+
+void System::pushBackEmptyModule() {
+	modules.push_back(Module());
+	//cout << "pushed back empty module" << endl; //DEBUG
+}
+
+void System::pushBackWareVersion(int moduleNo, Ware::Type wareType, 
+		double rel, double cost) {
+	switch(wareType) {
+		case Ware::Type::SW:
+			modules[moduleNo - 1].softVersions.push_back(
+				Software(rel, cost) );
+			break;
+		case Ware::Type::HW:
+			modules[moduleNo - 1].hardVersions.push_back(
+				Hardware(rel, cost) );
+			break;
+	}
+}
+
+int& System::curWareVersionNo(int moduleNo, Ware::Type wareType) {
+	switch(wareType) {
+		case Ware::Type::SW:
+			return modules[moduleNo - 1].curSoftVersionNo;
+		case Ware::Type::HW:
+			return modules[moduleNo - 1].curHardVersionNo;
+	}
+}
+int System::curWareVersionNo(int moduleNo, Ware::Type wareType) const {
+	switch(wareType) {
+		case Ware::Type::SW:
+			return modules[moduleNo - 1].curSoftVersionNo;
+		case Ware::Type::HW:
+			return modules[moduleNo - 1].curHardVersionNo;
+	}
+}
+
+const System::Ware& System::getCurWareVersion(int moduleNo, 
+		Ware::Type wareType) const {
+	return getWareVersion(moduleNo, wareType, 
+		curWareVersionNo(moduleNo, wareType) );
+}
+	
 double System::getRel() {
 	int i;
 	double rel = 1.0;
 	int nModules = getNModules();
 	for(i = 0; i < nModules; i++) {
-		rel *= getCurHardVersion(i).rel * getCurSoftVersion(i).rel;
+		rel *= getCurWareVersion(i, Ware::Type::SW).rel *
+			getCurWareVersion(i, Ware::Type::HW).rel;
 	}
 	return rel;
 }
@@ -123,7 +170,8 @@ double System::getCost() {
 	double cost = 0.0;
 	int nModules = getNModules();
 	for(i = 0; i < nModules; i++) {
-		cost += getCurHardVersion(i).cost + getCurSoftVersion(i).cost;
+		cost += getCurWareVersion(i, Ware::Type::SW).cost +
+			getCurWareVersion(i, Ware::Type::HW).cost;
 	}
 	return cost;
 }
@@ -146,28 +194,20 @@ void sysGenFromXml(System& system, const char* filename) {
 			iNode = iNode.next_sibling("module"), i++ )
 	{
 		system.pushBackEmptyModule();
-		const char* wareType[2] = {"hw", "sw"};
 		//cout << "in i-cycle\n"; //DEBUG
 		for(int k = 0; k < 2; k++) {
-			for(pugi::xml_node jNode = iNode.child(wareType[k]);
-					jNode;
-					jNode = jNode.next_sibling(wareType[k]) )
+			System::Ware::Type wareType = System::Ware::intToType(k);
+			const char* wareTypeStr = k == 0 ? "sw" : "hw"; // V replace with static
+			for(pugi::xml_node jNode = iNode.child(wareTypeStr);
+				jNode;
+				jNode = jNode.next_sibling(wareTypeStr) )
 			{
 				//cout << "in j-cycle1\n"; //DEBUG
 				string str1 = jNode.attribute("rel").value();
 				double rel = stod(str1);
 				string str2 = jNode.attribute("cost").value();
 				double cost = stod(str2);
-				//cout << "in j-cycle2\n"; //DEBUG
-				if(k == 0) {
-					//cout << "in j-cycle3h\n"; //DEBUG
-					system.pushBackHardVersion(i, rel, cost);
-					//cout << "in j-cycle4h\n"; //DEBUG
-				} else {
-					//cout << "in j-cycle3s\n"; //DEBUG
-					system.pushBackSoftVersion(i, rel, cost);
-					//cout << "in j-cycle4s\n"; //DEBUG
-				}
+				system.pushBackWareVersion(i, wareType, rel, cost);
 			}
 		}
 	}	
@@ -197,36 +237,55 @@ void sysSaveToXml(System& system, const char* filename) {
 //сохраняет данные о системе и всех версиях оборудования
 	pugi::xml_document doc;
     pugi::xml_node sysNode = doc.append_child("system");
-    sysNode.append_attribute("limitcost") = system.limitCost();
+    char buf[32];
+    sprintf(buf, "%.3g", system.limitCost() );
+    sysNode.append_attribute("limitcost") = buf;
     int nModules = system.getNModules();
     for(int i = 1; i <= nModules; i++) {
 		pugi::xml_node moduleNode = sysNode.append_child("module");
 		moduleNode.append_attribute("num") = i;
-		int nSoft = system.getNSoftVersions(i);
-		for(int j = 1; j <= nSoft; j++) {
-			pugi::xml_node wareNode = moduleNode.append_child("sw");
-			wareNode.append_attribute("num") = j;
-			char buf[64];
-			sprintf(buf, "%.3lf", system.getSoftVersion(i, j).rel);
-			wareNode.append_attribute("rel") = buf;
-			sprintf(buf, "%.3g", system.getSoftVersion(i, j).cost);
-			wareNode.append_attribute("cost") = buf;
-		}
-		int nHard = system.getNHardVersions(i);
-		for(int j = 1; j <= nHard; j++) {
-			pugi::xml_node wareNode = moduleNode.append_child("hw");
-			wareNode.append_attribute("num") = j;
-			char buf[64];
-			sprintf(buf, "%.3lf", system.getHardVersion(i, j).rel);
-			wareNode.append_attribute("rel") = buf;
-			sprintf(buf, "%.3g", system.getHardVersion(i, j).cost);
-			wareNode.append_attribute("cost") = buf;
+		for(int k = 0; k < 2; k++) {
+			System::Ware::Type wareType = System::Ware::intToType(k);
+			const char* wareTypeStr = k == 0 ? "sw" : "hw"; //V replace with static
+			int nWare = system.getNWareVersions(i, wareType);
+			for(int j = 1; j <= nWare; j++) {
+				pugi::xml_node wareNode = 
+					moduleNode.append_child(wareTypeStr);
+				wareNode.append_attribute("num") = j;
+				sprintf(buf, "%.3lf", 
+					system.getWareVersion(i, wareType, j).rel);
+				wareNode.append_attribute("rel") = buf;
+				sprintf(buf, "%.3g", 
+					system.getWareVersion(i, wareType, j).cost);
+				wareNode.append_attribute("cost") = buf;
+			}
 		}
 	}
     //doc.print(cout); //DEBUG
     doc.save_file(filename);
 }
 
+/*
+int findMinGEWareNo(const System& system, double val, int moduleNo, int wareType) {
+//find ware version with minimum rel. of those which >= val
+//in module(moduleNo)
+	int nVers;
+	if(wareType = hw) = system.getNHardVersions(moduleNo);
+	double min = 2.0;
+	double testVal;
+	int ans = 0;
+	for(int j = 1; j <= nVers; j++) {
+		testVal = system.getHardVersion(moduleNo, j).rel;
+		if(testVal < min && testVal >= val) {
+			min = testVal;
+			ans = j;
+		}
+	}
+}
+*/
+	
+//void findOptGenerous(System& system) {
+	
 int main(int argc, const char** argv) {
 	System system;
 	sysGenFromXml(system, "example.xml");
